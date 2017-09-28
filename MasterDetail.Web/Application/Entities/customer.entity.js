cc5ea91a9d5af717ef93e4155348be5335b45e8a
chrisjinet <32066835+chrisjinet@users.noreply.github.com>
2017-09-28 15:07:30 -0400

    Add files via upload

---
+++ MasterDetail.Web/Application/Entities/customer.entity.js
@@ -0,0 +1,16 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var transactionalinformation_entity_1 = require('./transactionalinformation.entity');
+var Customer = (function (_super) {
+    __extends(Customer, _super);
+    function Customer() {
+        _super.apply(this, arguments);
+    }
+    return Customer;
+}(transactionalinformation_entity_1.TransactionalInformation));
+exports.Customer = Customer;
+//# sourceMappingURL=customer.entity.js.map
---
+++ MasterDetail.Web/Application/Entities/transactionalinformation.entity.js
@@ -0,0 +1,8 @@
+"use strict";
+var TransactionalInformation = (function () {
+    function TransactionalInformation() {
+    }
+    return TransactionalInformation;
+}());
+exports.TransactionalInformation = TransactionalInformation;
+//# sourceMappingURL=transactionalinformation.entity.js.map
