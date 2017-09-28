3c47ab60c86426fc90930a8c4bd1b3e46b08322a
chrisjinet <32066835+chrisjinet@users.noreply.github.com>
2017-09-28 15:01:24 -0400

    Add files via upload

---
+++ MasterDetail.Data.EntityFramework/DataServices/UserDataService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MasterDetail.Interfaces;
+using MasterDetail.Business.Entities;
+
+namespace MasterDetail.Data.EntityFramework
+{
+   
+    /// <summary>
+    /// User Data Service
+    /// </summary>
+    public class UserDataService : EntityFrameworkService, IUserDataService
+    {
+        /// <summary>
+        /// Create User
+        /// </summary>
+        /// <param name="user"></param>
+        public void CreateUser(User user)
+        {
+            DateTime now = DateTime.Now;
+            user.DateCreated = now;
+            user.DateUpdated = now;
+            dbConnection.Users.Add(user);
+        }
+
+        /// <summary>
+        /// Update User
+        /// </summary>
+        /// <param name="user"></param>
+        public void UpdateUser(User user)
+        {
+            user.DateUpdated = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Get User
+        /// </summary>
+        /// <param name="emailAddress"></param>
+        /// <returns></returns>
+        public User GetUser(string emailAddress)
+        {
+            User user = dbConnection.Users.Where(u => u.EmailAddress == emailAddress).FirstOrDefault();
+            return user;
+        }
+
+        /// <summary>
+        /// Get User
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <returns></returns>
+        public User GetUser(int userID)
+        {
+            User user = dbConnection.Users.Where(u => u.UserID == userID).FirstOrDefault();
+            return user;
+        }
+
+    }
+}
---
+++ MasterDetail.Data.EntityFramework/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace MasterDetail.Data.EntityFramework.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MasterDetail.Data.EntityFramework.Angular2Database>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "MasterDetail.Data.EntityFramework.Angular2Database";
+        }
+
+        protected override void Seed(MasterDetail.Data.EntityFramework.Angular2Database context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
